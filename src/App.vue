b45dce58cdeaea3328218589c8ae5893045de6b2
Dmytro Boiko <iamboikodmytro@gmail.com>
2025-02-16 12:02:10 +0200

    Footer Navigation

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { CheckCircleIcon } from '@heroicons/vue/24/solid'
+import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
 </script>
 
 <template>
@@ -20,6 +21,42 @@ import { CheckCircleIcon } from '@heroicons/vue/24/solid'
       </div>
     </a>
   </header>
+
+  <main class="flex flex-col flex-grow"></main>
+
+  <footer class="sticky bottom-0 z-20 bg-white border-t">
+    <nav>
+      <ul class="flex items-center justify-around">
+        <li class="flex-1">
+          <a
+            class="flex flex-col justify-center items-center border p-2 text-xs capitalize"
+            href="#timeline"
+          >
+            <ClockIcon class="h-6 w-6" />
+            <span>timeline</span>
+          </a>
+        </li>
+        <li class="flex-1">
+          <a
+            class="flex flex-col justify-center items-center border p-2 text-xs capitalize"
+            href="#activities"
+          >
+            <ListBulletIcon class="h-6 w-6" />
+            <span>activities</span></a
+          >
+        </li>
+        <li class="flex-1">
+          <a
+            class="flex flex-col justify-center items-center border p-2 text-xs capitalize"
+            href="#progress"
+          >
+            <ChartBarIcon class="h-6 w-6" />
+            <span>progress</span></a
+          >
+        </li>
+      </ul>
+    </nav>
+  </footer>
 </template>
 
 <style scoped></style>
