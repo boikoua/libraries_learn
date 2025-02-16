8b8889f5044758623fe93a4b1130a81458a19363
Dmytro Boiko <iamboikodmytro@gmail.com>
2025-02-16 15:49:21 +0200

    active link

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { ref } from 'vue';
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline';
 import NavItem from './NavItem.vue';
 
@@ -20,12 +21,21 @@ const navItems = [
     icon: ChartBarIcon,
   },
 ];
+
+// Текущая страница
+const currentPage = ref('timeline');
 </script>
 
 <template>
   <nav>
     <ul class="flex items-center justify-around">
-      <NavItem v-for="item in navItems" :key="item.id" :href="`#${item.title}`">
+      <NavItem
+        v-for="item in navItems"
+        :key="item.id"
+        :href="`#${item.title}`"
+        :class="{ 'bg-gray-200 pointer-events-none': item.title === currentPage }"
+        @click="currentPage = item.title"
+      >
         <component :is="item.icon" class="h-6 w-6" />
         <span>{{ item.title }}</span>
       </NavItem>
