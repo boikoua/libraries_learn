e2b59eb88a51e617f26886a9ab76649e7c5e1cfe
Dmytro Boiko <iamboikodmytro@gmail.com>
2025-03-11 18:47:07 +0200

    libraries learn

--- src/components/TheHeader.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <header class="py-4">
-    <h1 class="text-[72px] text-center font-bold drop-shadow-2xl">
-      <span class="text-blue-700">On</span>
-      <span class="text-white">Track</span>
-    </h1>
-  </header>
-</template>
